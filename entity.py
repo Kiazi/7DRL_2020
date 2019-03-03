f7ed3acda6bd27cc55c9fd2582b8d894fd166d82
Kiazi <48168152+Kiazi@users.noreply.github.com>
2019-03-02 19:34:58 -0800

    Lots of stuff! Part 3 of tutorial almost done

--- entity.py
+++ entity.py
@@ -2,12 +2,12 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self,x,y,char,color):
+    def __init__(self, x, y, char, color):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
-    
+
     def move(self, dx, dy):
         # Move the entity by a given amount
         self.x += dx
