12c51958622264b972d61efae36ba4a06ecebeb6
Kiazi <48168152+Kiazi@users.noreply.github.com>
2019-03-04 21:20:48 -0800

    Monsters now chase the player and insult them!

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 import libtcodpy as libtcod
 
+from components.fighter import Fighter
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
@@ -35,7 +36,8 @@ def main():
         'light_ground': libtcod.Color(152, 251, 152)
     }
 
-    player = Entity(0,0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0,0, '@', libtcod.white, 'Player', blocks=True, fighter=fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -101,8 +103,8 @@ def main():
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The ' + entity.name + ' ponders the meaning of its existence.')
+                if entity.ai:
+                    entity.ai.take_turn(player, fov_map, game_map, entities)
                     
             game_state = GameStates.PLAYERS_TURN
 
